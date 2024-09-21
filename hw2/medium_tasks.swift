bf4e6b334e9483fc87ae9402162ba042ab6dba39
Zhannel Omarova <98833988+zhonniks@users.noreply.github.com>
2024-09-21 21:42:51 +0500

    medium

--- hw2/medium_tasks.swift
+++ hw2/medium_tasks.swift
@@ -1,19 +1,19 @@
-//task 1 - Set Intersec4on
+//task 1 
 var firstSet: Set<Int> = [1, 2, 3, 4]
 var secondSet: Set<Int> = [3, 4, 5, 6]
 var intersection: Set<Int>  = firstSet.intersection(secondSet)
 print(intersection)
 
-//task 2 - Dic4onary Update
+//task 2 
 var studentsScore: [String: Int] = [
     "Yernur": 100,
     "Olga": 95,
     "Aisha": 85
 ]
-studentsScore["Manarbek"] = 100
+studentsScore["Olga"] = 100
 print(studentsScore)
 
-//task 3 - Array Merge
+//task 3
 var firstArr: Array<String> = ["apple", "banana"]
 var secondArr: Array<String> = ["cherry", "date"]
 var thirdArr: Array<String> = firstArr + secondArr
